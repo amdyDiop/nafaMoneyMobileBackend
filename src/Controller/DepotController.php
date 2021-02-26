f6206e9ac83075d2a947f07ba14f6d0f7af57449
amdy diop <amdy@dofbi0.2>
2021-02-26 14:57:08 +0000

    depos done

---
+++ src/Controller/DepotController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AdminSysteme;
+use App\Entity\Comptes;
+use App\Entity\Depot;
+use App\Repository\AdminSystemeRepository;
+use App\Repository\CaissierRepository;
+use App\Repository\ComptesRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DepotController extends AbstractController
+{
+    private $compteRep;
+    private $adminSystemeRep;
+    private $caissierRep;
+    private $manager;
+
+    public function __construct(CaissierRepository $caissierRep, AdminSystemeRepository $adminSystemeRep, EntityManagerInterface $manager, ComptesRepository $compteRep)
+    {
+        $this->compteRep = $compteRep;
+        $this->caissierRep = $caissierRep;
+        $this->adminSystemeRep = $adminSystemeRep;
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route(path={"/api/depots/compte/{id}"},methods={"POST"},
+     * )
+     */
+    public function depot(int $id ,Request  $request): Response
+    {
+        $compte = $this->compteRep->find($id);
+        $depot = new Depot();
+        $data = json_decode($request->getContent(),true);
+        $depot->setMontant($data['montant']);
+        if ($compte) {
+            $user = $this->getUser();
+            $username = $user->getUsername();
+            if ($user instanceof AdminSysteme) {
+                $user = $this->adminSystemeRep->findBy(['email' => $username]);
+                $depot->setAdminSysteme($user[0]);
+            } else {
+                $user =$this->adminSystemeRep->findBy(['email'=>$username]);
+                $depot->setAdminSysteme($user[0]);
+            }
+            $solde = $depot->getMontant() + $compte->getSolde();
+            $compte->setSolde($solde);
+            $depot->setCompte($compte);
+        }
+        else {
+            return new JsonResponse("le compte avec l'id: $id n'existe pas", Response::HTTP_BAD_REQUEST, []);
+        }
+        $this->manager->persist($depot);
+        $this->manager->flush();
+        return new JsonResponse($data['montant']." a été déposé sur le compte" , Response::HTTP_CREATED, []);
+    }
+}
--- src/Entity/Depot.php
+++ src/Entity/Depot.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\DepotRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +11,52 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=DepotRepository::class)
+ * @ApiResource(
+ *     attributes={"pagination_partial"=true,"pagination_client_items_per_page"=true},
+ *     collectionOperations={
+ *       "post"=
+ *       {
+ *          "method"="post",
+ *          "path"="/depots/compte/{id}",
+ *          "attributes"={"security"="is_granted('ROLE_Admin') or is_granted('ROLE_Caissier')",
+ *          "security_message"="Vous n'avez pas access à cette Ressource"},
+ *          "denormalization_context"={"groups"={"depot:add"}},
+ *       },
+ *       "get"=
+ *       {
+ *          "method"= "get",
+ *          "path"="admin/caissiers",
+ *          "normalization_context"={"groups"={"caissier:liste"}},
+ *      "attributes"={"security"="is_granted('ROLE_Admin')",
+ *          "security_message"="Vous n'avez pas access à cette Ressource"},
+ *       },
+ *     },
+ *     itemOperations={
+ *          "get"=
+ *              {
+ *                "methode"="get",
+ *                "path"="caissier/{id}",
+ *                "security"="is_granted('ROLE_Admin') or object.getId() == user.id",
+ *                "security_message"="Vous n'avez pas access à cette Ressource",
+ *                 "normalization_context"={"groups"={"caissier"}},
+ *                },
+ *              "put"=
+ *              {
+ *                "methode"="put",
+ *                "path"="admin/caissier/{id}",
+ *                "security"="is_granted('ROLE_Admin')",
+ *                "security_message"="Vous n'avez pas access à cette Ressource",
+ *                "denormalization_context"={"groups"={"edit:caissier"}},
+ *                },
+ *             "delete"=
+ *               {
+ *                "methode"="delete",
+ *                "path"="admin/caissier/{id}",
+ *                "security"="is_granted('ROLE_Admin')",
+ *                "security_message"="Vous n'avez pas access à cette Ressource",
+ *                },
+ *      },
+ * )
  */
 class Depot
 {
@@ -23,7 +70,7 @@ class Depot
 
     /**
      * @ORM\ManyToOne(targetEntity=Caissier::class, inversedBy="depots")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=true)
      */
     private $caissier;
 
@@ -39,6 +86,20 @@ class Depot
      */
     private $adminSysteme;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+    /**
+     * @ORM\Column(type="integer")
+     * @Groups({"depot:add"})
+     */
+    private $montant;
+
+    public function __construct()
+    {
+        $this->createdAt = new  \DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -76,6 +137,30 @@ class Depot
     public function setAdminSysteme(?AdminSysteme $adminSysteme): self
     {
         $this->adminSysteme = $adminSysteme;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(int $montant): self
+    {
+        $this->montant = $montant;
 
         return $this;
     }
